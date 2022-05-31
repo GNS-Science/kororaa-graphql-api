26be35490ada6c80796be7bdc6e4fa8d2dfbac0f
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-05-31 14:26:27 +1200

    added about resolver; added test; setup dependencies;

---
+++ kororaa_graphql_api/schema/schema.py
@@ -0,0 +1,18 @@
+"""The main API schema."""
+
+import graphene
+from graphene import relay
+
+
+class QueryRoot(graphene.ObjectType):
+    """This is the entry point for all graphql query operations"""
+
+    node = relay.Node.Field()
+
+    about = graphene.String(description='About this API ')
+
+    def resolve_about(root, info, **args):
+        return "Hello World, I am kororaa_graphql_api!"
+
+
+schema_root = graphene.Schema(query=QueryRoot, mutation=None, auto_camelcase=False)
--- tests/test_kororaa_graphql_api.py
+++ tests/test_kororaa_graphql_api.py
@@ -1,21 +1,43 @@
-#!/usr/bin/env python
 """Tests for `kororaa_graphql_api` package."""
 
-import pytest
+# from unittest import mock
+
+import datetime as dt
+import unittest
+
+from dateutil.tz import tzutc
+
+from graphene.test import Client
+
+from kororaa_graphql_api.schema import schema_root
+from kororaa_graphql_api.kororaa_graphql_api import create_app
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+class TestFlaskApp(unittest.TestCase):
+    """Tests the basic app create."""
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+    def test_create_app(self):
+        app = create_app()
+        print(app)
+        assert app
+
+
+class TestSchemaAboutResolver(unittest.TestCase):
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    A simple `About` resolver returns some metadata about the API.
+    """
 
+    def setUp(self):
+        self.client = Client(schema_root)
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-    del response
+    def test_get_about(self):
+
+        QUERY = """
+        query {
+            about
+        }
+        """
+
+        executed = self.client.execute(QUERY, variable_values=dict(created=dt.datetime.now(tzutc())))
+        print(executed)
+        self.assertTrue('Hello World' in executed['data']['about'])
