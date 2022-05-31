545120b5e0bd2a1407e5836c634502ce45ac85b6
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-06-01 05:48:30 +1200

    basic query working in unit tests

--- kororaa_graphql_api/schema/toshi_hazard.py
+++ kororaa_graphql_api/schema/toshi_hazard.py
@@ -1,59 +1,19 @@
-"""The main API schema."""
-
 import graphene
 from graphene import relay
-from toshi_hazard_store import query
 
-class ToshiHazardCurve(graphene.ObjectType):
-    """Represents one set of level and values for a hazard curve."""
-    levels = graphene.List(graphene.Float, description="IMT levels.")
-    values = graphene.List(graphene.Float, description="Hazard values.")
+class ToshiHazardCurveQuery(graphene.ObjectType):
+    """query hazard curves."""
 
-
-class ToshiHazardResult(graphene.ObjectType):
-    """All the info about a given curve
-    loc
-    imt
-    agg
-    rlz
-    vs30
-    """
-    toshi_hazard_id = graphene.ID()
-    loc = graphene.String()
-    imt = graphene.String()
-    agg = graphene.String()
-
-    curve = graphene.Field(ToshiHazardCurve)
-
-# class ToshiHazardCurveConnection(relay.Connection):
-#     """Relay Connection class."""
-
-#     class Meta:
-#         node = ToshiHazardResult
-
-class ToshiHazardCurveResult(graphene.ObjectType):
-    ok = graphene.Boolean()
-    #curves_relay = relay.ConnectionField(ToshiHazardCurveConnection)
-    curves = graphene.List(ToshiHazardResult)
-
-
-class QueryRoot(graphene.ObjectType):
-    """This is the entry point for all graphql query operations"""
-
-    node = relay.Node.Field()
-
-    about = graphene.String(description='About this API ')
-
-    #hazard_curves = graphene.Field(ToshiHazardCurveQuery)
-
-    hazard_curves = graphene.Field(ToshiHazardCurveResult,
+    args = graphene.Field(ToshiHazardCurveResult,
         toshi_hazard_id = graphene.Argument(graphene.ID),
         imts = graphene.Argument(graphene.List(graphene.String)),
         locs = graphene.Argument(graphene.List(graphene.String)),
         aggs =graphene.Argument(graphene.List(graphene.String))
         )
 
-    def resolve_hazard_curves(root, info, **kwargs):
+    qry = graphene.Field(ToshiHazardCurveResult)
+
+    def resolve_query(root, info, **kwargs):
         print(f"resolve_hazard_curves(root, info, **kwargs) {kwargs}")
         print("#res = list(query.get_hazard_stats_curves(TOSHI_ID, ['PGA'], ['WLG', 'QZN'], ['mean']))")
         res = list(query.get_hazard_stats_curves(
@@ -83,10 +43,3 @@ class QueryRoot(graphene.ObjectType):
     #     # db_metrics.put_duration(__name__, 'resolve_search' , dt.utcnow()-t0)
     #     # return Search(ok=True, search_result=search_result)
 
-
-
-    def resolve_about(root, info, **args):
-        return "Hello World, I am kororaa_graphql_api!"
-
-
-schema_root = graphene.Schema(query=QueryRoot, mutation=None, auto_camelcase=False)
