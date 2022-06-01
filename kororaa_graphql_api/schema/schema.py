45020cc1f9112837eed70205ce71c2aa2984e443
Chris B Chamberlain <chrisbc@artisan.co.nz>
2022-06-01 14:29:22 +1200

    Feature/5 hazard result (#8)
    * basic query working in unit tests
    * working on AWS with pynamodb service

--- kororaa_graphql_api/schema/schema.py
+++ kororaa_graphql_api/schema/schema.py
@@ -2,6 +2,44 @@
 
 import graphene
 from graphene import relay
+from toshi_hazard_store import query
+
+
+class ToshiHazardCurve(graphene.ObjectType):
+    """Represents one set of level and values for a hazard curve."""
+
+    levels = graphene.List(graphene.Float, description="IMT levels.")
+    values = graphene.List(graphene.Float, description="Hazard values.")
+
+
+class ToshiHazardResult(graphene.ObjectType):
+    """All the info about a given curve
+    loc
+    imt
+    agg
+    rlz
+    vs30
+    """
+
+    toshi_hazard_id = graphene.ID()
+    loc = graphene.String()
+    imt = graphene.String()
+    agg = graphene.String()
+
+    curve = graphene.Field(ToshiHazardCurve)
+
+
+# class ToshiHazardCurveConnection(relay.Connection):
+#     """Relay Connection class."""
+
+#     class Meta:
+#         node = ToshiHazardResult
+
+
+class ToshiHazardCurveResult(graphene.ObjectType):
+    ok = graphene.Boolean()
+    # curves_relay = relay.ConnectionField(ToshiHazardCurveConnection)
+    curves = graphene.List(ToshiHazardResult)
 
 
 class QueryRoot(graphene.ObjectType):
@@ -10,6 +48,48 @@ class QueryRoot(graphene.ObjectType):
     node = relay.Node.Field()
 
     about = graphene.String(description='About this API ')
+
+    # hazard_curves = graphene.Field(ToshiHazardCurveQuery)
+
+    hazard_curves = graphene.Field(
+        ToshiHazardCurveResult,
+        toshi_hazard_id=graphene.Argument(graphene.ID),
+        imts=graphene.Argument(graphene.List(graphene.String)),
+        locs=graphene.Argument(graphene.List(graphene.String)),
+        aggs=graphene.Argument(graphene.List(graphene.String)),
+    )
+
+    def resolve_hazard_curves(root, info, **kwargs):
+        print(f"resolve_hazard_curves(root, info, **kwargs) {kwargs}")
+        print("#res = list(query.get_hazard_stats_curves(TOSHI_ID, ['PGA'], ['WLG', 'QZN'], ['mean']))")
+        res = list(
+            query.get_hazard_stats_curves(
+                kwargs.get('toshi_hazard_id'), kwargs.get('imts'), kwargs.get('locs'), kwargs.get('aggs')
+            )
+        )
+
+        def get_curve(obj):
+            # print(f"get_curve values from {obj}")
+            levels, values = [], []
+            for lv in obj.values:
+                levels.append(float(lv.lvl))
+                values.append(float(lv.val))
+            return ToshiHazardCurve(levels=levels, values=values)
+
+        def build_response_from_query(query_response):
+            # print("build_response_from_query", query_response)
+            # print()
+            for obj in query_response:
+                yield ToshiHazardResult(
+                    toshi_hazard_id=obj.haz_sol_id, imt=obj.imt, loc=obj.loc, agg=obj.agg, curve=get_curve(obj)
+                )
+
+        return ToshiHazardCurveResult(ok=True, curves=build_response_from_query(res))
+
+    #     # t0 = dt.utcnow()
+    #     # search_result = db_root.search_manager.search(kwargs.get('search_term'))
+    #     # db_metrics.put_duration(__name__, 'resolve_search' , dt.utcnow()-t0)
+    #     # return Search(ok=True, search_result=search_result)
 
     def resolve_about(root, info, **args):
         return "Hello World, I am kororaa_graphql_api!"
--- tests/test_kororaa_graphql_api.py
+++ tests/test_kororaa_graphql_api.py
@@ -2,15 +2,17 @@
 
 # from unittest import mock
 
-import datetime as dt
+# import datetime as dt
 import unittest
+from unittest import mock
 
-from dateutil.tz import tzutc
+# from dateutil.tz import tzutc
 
 from graphene.test import Client
 
 from kororaa_graphql_api.schema import schema_root
 from kororaa_graphql_api.kororaa_graphql_api import create_app
+from toshi_hazard_store import model
 
 
 class TestFlaskApp(unittest.TestCase):
@@ -38,6 +40,67 @@ class TestSchemaAboutResolver(unittest.TestCase):
         }
         """
 
-        executed = self.client.execute(QUERY, variable_values=dict(created=dt.datetime.now(tzutc())))
+        executed = self.client.execute(QUERY)
         print(executed)
         self.assertTrue('Hello World' in executed['data']['about'])
+
+
+def mock_query_response(*args, **kwargs):
+
+    lvps = list(map(lambda x: model.LevelValuePairAttribute(lvl=x / 1e3, val=(x / 1e6)), range(1, 11)))
+    # print(lvps)
+
+    obj = model.ToshiOpenquakeHazardCurveStats(
+        haz_sol_id="ABCDE",
+        imt_loc_agg_rk="350:SA(0.5):WLG:0.1",
+        loc="WLG",
+        agg="0.1",
+        imt="SA(0.5)",
+        values=lvps,
+    )
+    return [obj, obj, obj]
+
+
+@mock.patch('toshi_hazard_store.query.get_hazard_stats_curves', side_effect=mock_query_response)
+class TestHazardCurvesResolver(unittest.TestCase):
+    """
+    The masthead feature of this  API.
+    """
+
+    def setUp(self):
+        self.client = Client(schema_root)
+
+    def test_get_about(self, mocked_qry):
+
+        QUERY = """
+        query {
+            hazard_curves (
+                toshi_hazard_id: "ABCDE"
+                imts: ["PGA", "SA(0.5)"]
+                locs: ["WLG", "QZN"]
+                aggs: ["0.1", "mean"]
+                )
+            {
+                ok
+                curves {
+                    toshi_hazard_id
+                    imt
+                    loc
+                    agg
+                    curve {
+                        levels
+                        values
+                    }
+                }
+            }
+        }
+        """
+
+        executed = self.client.execute(QUERY)
+        print(executed)
+        res = executed['data']['hazard_curves']
+
+        self.assertEqual(mocked_qry.call_count, 1)
+        self.assertEqual(res['ok'], True)
+        self.assertEqual(len(res['curves']), 3)
+        self.assertEqual(res['curves'][0]['toshi_hazard_id'], "ABCDE")
