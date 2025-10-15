4bda4cf4a1a8414a557aec45dd9f2b8ec49cb55d
Chris B Chamberlain <chrisbc@artisan.co.nz>
2025-10-16 10:38:04 +1300

    update tests for new moto API; `poetry update/sync`;

--- tests/test_textual_content.py
+++ tests/test_textual_content.py
@@ -4,7 +4,7 @@ import boto3
 import io
 import dateutil.parser
 from graphene.test import Client
-from moto import mock_s3
+from moto import mock_aws
 from pathlib import Path, PurePath
 from kororaa_graphql_api.schema import schema_root
 from kororaa_graphql_api.config import S3_BUCKET_NAME, TEXT_CONTENT_INDEX_KEY, TEXT_CONTENT_FOLDER_KEY
@@ -26,18 +26,18 @@ def setup_content():
 
 
 class TestTextContentS3(unittest.TestCase):
-    mock_s3 = mock_s3()
+    mock_aws = mock_aws()
 
     def setUp(self):
         self.client = Client(schema_root)
-        self.mock_s3.start()
+        self.mock_aws.start()
         self._s3 = boto3.resource('s3', region_name='us-east-1')
         self._s3.create_bucket(Bucket=S3_BUCKET_NAME)
         self._bucket = self._s3.Bucket(S3_BUCKET_NAME)
         setup_content()
 
     def tearDown(self):
-        self.mock_s3.stop()
+        self.mock_aws.stop()
 
     def test_s3_create_textual_index(self):
 
